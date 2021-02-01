568d4b4e0e99e52a775a29ca4c84d68c52c21b42
Amit Apollo Barman <ambarman@cisco.com>
2021-02-01 03:00:58 -0800

    Initial Commit

---
+++ thermals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  thermals
+//
+//  Created by Amit Apollo Barman on 2/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
