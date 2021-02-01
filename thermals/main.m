5a8e96a4e7343a0bc7f4c764a7178056db79ccdb
Amit Apollo Barman <ambarman@cisco.com>
2021-02-01 03:41:21 -0800

    initial commit
    initial commit of SMC sensors fiir Mac.

--- thermals/main.m
+++ thermals/main.m
@@ -6,11 +6,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "SMCWrapper.h"
+
+NSString *get_cpu_temperature(int core_number)
+{
+    SMCWrapper *smc = [SMCWrapper sharedWrapper];
+    NSNumber *temp;
+    if ( [smc readKey:"TC0D" intoNumber:&temp] ){
+       return (void)(@"[%d] CPU Temperature:\t %@"), (void)(core_number), [temp stringValue];
+    }
+    else return @"CPU Temperature: UNKNOWN";
+}
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    NSLog(@"%@", get_cpu_temperature(0));
+    
     return 0;
 }
+
+
