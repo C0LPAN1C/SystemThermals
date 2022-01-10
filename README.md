be714ee728ef690c8695b443c88182eee698d728
Amit Apollo Barman <me@apollosoftware.org>
2022-01-10 12:42:08 -0500

    icons and m1 branch
    icons and m1 branch

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# System Thermals
-## SMC Sensors for CPU Temp (degrees)
+# System Thermals for M1
+## Sensors for CPU Temp (degrees)
 
-Uses SMC Controller to read macOS Thermal Sensors' Proximities (CPU temp). Loads in system tray/task tray, updates based on temperature, and processes CPU temp.
+Uses IOKit to read macOS Thermal Sensors' Proximities (CPU temp). Loads in system tray/task tray, updates based on temperature, and processes CPU temp.
+
+<img width="116" alt="Screen Shot 2021-12-28 at 5 19 58 PM" src="https://user-images.githubusercontent.com/654661/147611347-52191200-bfb2-4f5c-aab0-71f57cd697f5.png">
+
 
 ## Requirements:
-- macOS 10.11.x +
-- Xcode 10.0 + 
-- IOKit permissions in entitlements
+- M1 Mac
+- macOS 10.14.x +
+- Xcode 13.0 + 
 - XPC for full implementation
 
 ## Contact:
--- SystemThermals/AppDelegate.m
+++ SystemThermals/AppDelegate.m
@@ -6,22 +6,34 @@
 //
 
 #import "AppDelegate.h"
-#import "M1Wrapper.h"
+
 
 @interface AppDelegate ()
-
-
+@property (strong, nonatomic) M1Wrapper *m1smc;
 @end
 
 @implementation AppDelegate
 
+int t_UNIT = CELC;
 
-- (NSColor *) get_temp_colour
+
+-(id)init
+{
+    self = [super init];
+    if(self)
+    {
+        _m1smc = [M1Wrapper sharedWrapper];
+        t_UNIT = CELC;
+    }
+
+    return self;
+}
+
+- (NSColor *) get_temp_colour:(float) t
 {
     NSColor *tempColour = [NSColor textColor];
-    float t = [self get_cpu_temp];
     
-    if (t < TJ_MID)
+    if (t < TJ_WARN1)
     {
         tempColour = [NSColor greenColor];
     }
@@ -43,20 +55,91 @@
 
 - (NSString *) get_cpu_temperature
 {
-    M1Wrapper *m1smc = [M1Wrapper sharedWrapper];
-    return [m1smc get_temp_values];
+    return [_m1smc get_temp_values];
 }
 
 - (float) get_cpu_temp
 {
-    M1Wrapper *m1smc = [M1Wrapper sharedWrapper];
-    return [m1smc get_temp_float];
+    return [_m1smc get_temp_float];
+}
+
+-(void) open
+{
+    
+}
+
+- (void)showMenu {
+ 
+    NSMenuItem* menuBarItem = [[NSMenuItem alloc]
+                            initWithTitle:@"Custom" action:NULL keyEquivalent:@""];
+    // title localization is omitted for compactness
+    _menu = [[NSMenu alloc] initWithTitle:@"Custom"];
+    [menuBarItem setSubmenu:_menu];
+    [[NSApp mainMenu] insertItem:menuBarItem atIndex:3];
+}
+
+- (void)exitApp{
+    NSLog(@"Quitting App");
+    exit(0);
+}
+
+- (IBAction)selectDefault:(id)sender {
+    [self selectUnit:NONE];
+}
+- (IBAction)selectCelcius:(id)sender {
+    [self selectUnit:CELC];
+}
+- (IBAction)selectFahrenheit:(id)sender {
+    [self selectUnit:FAREN];
+}
+- (IBAction)selectKelvin:(id)sender {
+    [self selectUnit:KELV];
+}
+
+- (void)selectUnit:(int) unit{
+    NSLog(@"selecUnit");
+    t_UNIT = unit;
+    [self update_temp];
+}
+
+- (void)selectUnit{
+    NSLog(@"selecUnit");
+    if (t_UNIT == NONE)
+        t_UNIT = CELC;
+    else if (t_UNIT == FAREN)
+        t_UNIT = KELV;
+    else if (t_UNIT == KELV)
+        t_UNIT = NONE;
+    else if (t_UNIT == CELC)
+        t_UNIT = FAREN;
+    [self update_temp];
+}
+
+-(void)buttonClick:(id)sender {
+    NSEvent* event;
+
+    event = [NSEvent mouseEventWithType:NSEventTypeLeftMouseDown
+                               location:[NSEvent mouseLocation]
+                             modifierFlags:[event modifierFlags]
+                                 timestamp:CFAbsoluteTimeGetCurrent()
+                              windowNumber:[event windowNumber]
+                                   context:nil
+                               eventNumber:[event eventNumber]
+                                clickCount:[event clickCount]
+                                  pressure:[event pressure]];
+    
+    [NSMenu popUpContextMenu:_menu withEvent:event forView:(NSButton *)sender];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     [NSApp setActivationPolicy: NSApplicationActivationPolicyProhibited];
     _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    
+    _menu = [NSApp mainMenu];
+    _statusItem.button.action = @selector(buttonClick:);
+    _statusItem.button.target = self;
+    _statusBar = [NSStatusBar systemStatusBar];
+
+    [_statusBar statusItemWithLength:3];
     // The text that will be shown in the menu bar
     [self update_temp];
     
@@ -70,18 +153,38 @@
     [self performSelector:@selector(mainthread_dotask) withObject:nil afterDelay:DELAY_T];
 }
 
+- (float) get_temp_var:(float) t {
+
+    if (t_UNIT == CELC)
+        return t;
+    if (t_UNIT == KELV)
+        return t + 273.15f;
+    return (t * 9.0f/5.0f) + 32.0f; //default
+}
+
+- (NSString *) get_temp_label {
+
+    if (t_UNIT == KELV)
+        return @"K";
+    if (t_UNIT == FAREN)
+        return @"F";
+    if (t_UNIT == NONE)
+        return @"";
+    return @"C";
+}
 
 - (void) update_temp
 {
-    NSDictionary *titleAttributes = [NSDictionary dictionaryWithObject:[self get_temp_colour] forKey:NSForegroundColorAttributeName];
-    
-    NSAttributedString* colouredTitle = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%.1f°",[self get_cpu_temp]] attributes:titleAttributes];
+    float t = [self get_cpu_temp];
+    NSDictionary *titleAttributes = [NSDictionary dictionaryWithObject:[self get_temp_colour:t] forKey:NSForegroundColorAttributeName];
+
+    NSAttributedString* colouredTitle = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%.1f°%@",[self get_temp_var:t],[self get_temp_label]] attributes:titleAttributes];
 
     _statusItem.button.attributedTitle = colouredTitle;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    [_m1smc m1Close];
 }
 
 
